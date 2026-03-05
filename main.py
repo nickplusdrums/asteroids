7876b2243c9eb3b3fe536c1e2df517f0bc298eb2
nickplusdrums <nickplusdrums@gmail.com>
2026-03-04 22:03:36 -0800

    Add FPS Constraint to Screen

--- main.py
+++ main.py
@@ -9,14 +9,18 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                return    
             pass
         screen.fill("black")
         pygame.display.flip()
+        dt = (clock.tick(60) / 1000)
+
 
 
 

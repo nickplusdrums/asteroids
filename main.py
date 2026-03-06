15038c6114ebffbf8e3e711b7084a9b2fbc9d0dd
nickplusdrums <nickplusdrums@gmail.com>
2026-03-05 19:19:54 -0800

    Add Update and Draw groups to main game loop

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from player import Player
 
 def main():
     pygame.init()
+   
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -18,8 +22,9 @@ def main():
                 return    
             
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
 
         dt = (clock.tick(60) / 1000)
